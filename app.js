cc75b6abfab6bdde6c221b3dd39d30a0543dc357
jeniahc <123027522+jeniahc@users.noreply.github.com>
2024-03-21 07:31:28 -0500

    update geoloaction

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ document.getElementById("demo0").innerHTML = localStorage.getItem("name");
 const x = document.getElementById("demo1");
 
 function getLocation() {
-  try {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } catch {
-    x.innerHTML = err;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
 
@@ -15,6 +15,7 @@ function showPosition(position) {
   x.innerHTML = "Latitude: " + position.coords.latitude + 
   "<br>Longitude: " + position.coords.longitude;
 }
+
 
 function showError(error) {
     switch(error.code) {
